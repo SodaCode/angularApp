65de4300467ce8eb12f87bb8ceda1369d0514dd9
SodaCode <dominicsaechao1@gmail.com>
2019-10-05 11:25:36 -0700

    added constructor and made method calls within

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,6 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  templateUrl: './user.component.html'
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
 })
-export class UserComponent {}
+export class UserComponent {
+  // properties
+  firstName = 'John';
+  lastName = 'Doe';
+  age = 30;
+
+  // method
+  constructor() {
+    // this.sayHello();
+    console.log(this.age);
+    this.hasBirthday();
+    console.log(this.age);
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`);
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
+}
