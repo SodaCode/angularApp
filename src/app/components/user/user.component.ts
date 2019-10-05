96684a49030be17713f0dd1c9e7afb4fd59cf4d2
SodaCode <dominicsaechao1@gmail.com>
2019-10-05 14:19:27 -0700

    added OnInit to user files

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,29 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   // properties
-  firstName = 'John';
-  lastName = 'Doe';
-  age = 30;
+  user: User;
 
   // method
   constructor() {
-    // this.sayHello();
-    console.log(this.age);
-    this.hasBirthday();
-    console.log(this.age);
   }
 
-  sayHello() {
-    console.log(`Hello ${this.firstName}`);
-  }
-
-  hasBirthday() {
-    this.age += 1;
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 Main St',
+        city: 'Boston',
+        state: 'MA'
+      }
+    }
   }
 }
+
