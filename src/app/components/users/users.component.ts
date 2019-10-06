24ecb835c1911ec33eb0ba89014a64643030b0f9
SodaCode <dominicsaechao1@gmail.com>
2019-10-05 19:03:38 -0700

    added Users component and show extend

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 Main St',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Hartts',
+          age: 34,
+          address: {
+            street: '20 Spool St',
+            city: 'Lindmast',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 26,
+          address: {
+            street: '55 Millard St',
+            city: 'Miami',
+            state: 'FL'
+          }
+        }
+      ];
+      this.loaded = true;
+    }, 2000);
+
+    // this.addUser({
+    //   firstName: 'David',
+    //   lastName: 'Jackson',
+    //   age: 44,
+    //   address: {
+    //     street: '22 Dillard St',
+    //     city: 'Orlando',
+    //     state: 'FL'
+    //   }
+    // });
+
+    this.showExtended = false;
+  }
+
+
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
