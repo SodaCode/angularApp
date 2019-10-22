60ea0ebd92c0ac10c70e47e4489af180967f68bd
SodaCode <dominicsaechao1@gmail.com>
2019-10-22 13:24:30 -0700

    Added a post section, post model, post component, and a post-form component. Currently, adding a post throught the form will allow an array to be made, but the post will not be seen in the html of the post section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/Forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { UsersService } from './services/users.service';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostService } from './services/post.service';
+import { PostFormComponent } from './components/post-form/post-form.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [UsersService],
+  declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent, PostsComponent, PostFormComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [UsersService, PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
